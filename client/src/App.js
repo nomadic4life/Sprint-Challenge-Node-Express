4403807f8eb794f215cc1e6db3e23b4c88556e23
Dennis Orbison <orbison.dennis@gmail.com>
2019-01-11 10:44:55 -0800

    link react app to server. and is working, ITS WORKING

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state ={
+    projects: [],
+    actions: [],
+  }
+
+  componentDidMount() {
+    axios('http://localhost:8000/projects')
+    .then(projects => this.setState({projects: projects.data}))
+    .catch(err => console.log(err));
+
+    axios('http://localhost:8000/actions')
+    .then(actions => this.setState({actions: actions.data}))
+    .catch(err => console.log(err));
+  }
+
   render() {
+    console.log(this.state.projects)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.projects.map(project => {
+          return (
+            <React.Fragment key = {project.id}>
+              <h1>name: {project.name}</h1>
+              <h3>description: {project.description}</h3>
+            </React.Fragment>
+          )
+        })}
+
+        {this.state.actions.map(action => {
+          return (
+            <React.Fragment key = {action.id}>
+              <h4>notes: {action.notes}</h4>
+              <h4>description: {action.description}</h4>
+            </React.Fragment>
+          )
+        })}
       </div>
     );
   }
